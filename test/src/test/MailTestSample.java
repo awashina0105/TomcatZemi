87b54cf6a71638e038096b2cbc1d9e0848aa557e
awashina0105 <suzuneandmisato1129@gmail.com>
2019-11-18 11:29:06 +0900

    comit

---
+++ test/src/test/MailTestSample.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MailTestSample {
+
+	  public void main(String args[]) throws InterruptedException {
+	    // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+	    System.setProperty("webdriver.chrome.driver", "C:/S3A1_07/pleiades/tomcat/8/lib/chromedriver_win32");
+
+	    WebDriver driver = new ChromeDriver();
+	    driver.get("http://www.google.com/");
+	    Thread.sleep(5000);  // Let the user actually see something!
+	    WebElement searchBox = driver.findElement(By.name("q"));
+	    searchBox.sendKeys("ChromeDriver");
+	    searchBox.submit();
+	    Thread.sleep(5000);  // Let the user actually see something!
+	    driver.quit();
+	  }
+
+
+}
