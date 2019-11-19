fe705e004ed10577e07454fbde27ed045514ac04
awashina0105 <suzuneandmisato1129@gmail.com>
2019-11-19 14:32:37 +0900

    comit

--- test/src/createFile/Create.java
+++ test/src/createFile/Create.java
@@ -1,7 +1,31 @@
 package createFile;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Create {
 	
+	private FileWriter fw ;
 	
+	public boolean fileCreate(String userName){
+		
+		CreateRandom random = new CreateRandom();
+		int randomId = random.random();
+
+		
+		try{
+			
+			fw = new FileWriter("./" + userName + "/" + randomId + ".html");
+			
+			
+			
+		}catch (IOException e) {
+			// TODO: handle exception
+		}
+		
+		
+		return false;
+		
+	}
 
 }
