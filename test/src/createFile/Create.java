d2cc7a2a4dcfda5ae3cf566a90f6018ce76a81a2
awashina0105 <suzuneandmisato1129@gmail.com>
2019-11-22 11:53:51 +0900

    comit

--- test/src/createFile/Create.java
+++ test/src/createFile/Create.java
@@ -9,6 +9,7 @@ import java.util.Calendar;
 public class Create {
 
 	private static FileWriter fw ;
+	private static final String PASS ="Z:/git/TomcatZemi/test/WebContent/";
 
 	public static void main (String args[]) {
 
@@ -21,7 +22,7 @@ public class Create {
 		SimpleDateFormat sd = new SimpleDateFormat("yyyy_MM_dd");
 		String toDay = sd.format(cal.getTime());
 
-		File file = new File("Z:/git/TomcatZemi/test/WebContent/" + toDay);
+		File file = new File(PASS + toDay);
 
 
 
@@ -33,14 +34,11 @@ public class Create {
             file.mkdir();
         }
 
-
 		try{
-
-			file = new File("Z:/git/TomcatZemi/test/WebContent/" + toDay + "/" + randomId + ".html");
+			file = new File(PASS + toDay + "/" + randomId + ".html");
 			file.createNewFile();
 
-
-			fw = new FileWriter("Z:/git/TomcatZemi/test/WebContent/" + toDay + "/" + randomId + ".html");
+			fw = new FileWriter(PASS + toDay + "/" + randomId + ".html");
 
 			fw.write("テスト");
             fw.close();
@@ -56,9 +54,7 @@ public class Create {
             System.out.println("ファイルは存在します");
         }else{
             System.out.println("ファイルは存在しません");
-
         }
-
 
 
 	}
